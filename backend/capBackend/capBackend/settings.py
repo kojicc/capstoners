f318c843d9e3a006689aae6d1a3ff936b1b7caef
kojicc <syjeikoo@gmail.com>
2024-10-22 16:23:14 +0800

    Update settings.py

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -109,7 +109,6 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
