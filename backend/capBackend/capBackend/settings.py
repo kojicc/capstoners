808e4e6b1e10bd7e321158051aeec69d6f9390f1
kojicc <syjeikoo@gmail.com>
2024-10-22 17:23:02 +0800

    Update settings.py

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -154,7 +154,7 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
@@ -223,8 +223,9 @@ FRONTEND_URL = 'http://localhost:3000'
 
 # Static files settings
 # STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # If you ever use static files
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)  # If you ever use static files
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
