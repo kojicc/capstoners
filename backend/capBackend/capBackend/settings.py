3a9310201cee376dc88a6b115e065c122d8d4028
kojicc <syjeikoo@gmail.com>
2024-10-24 00:22:53 +0800

    qwe

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import datetime, timedelta
 # from dotenv import load_dotenv
 import os
-import django_heroku
+import django_heroku # type: ignore
 import dj_database_url
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
