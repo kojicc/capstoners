0aa21e69c629f967f30906fee6f3942dcfc81548
kojicc <syjeikoo@gmail.com>
2024-10-25 23:13:09 +0800

    Update views.py

--- backend/capBackend/products/views.py
+++ backend/capBackend/products/views.py
@@ -150,7 +150,16 @@ class ExportImportProductView(APIView):
             
             for product in products:
                 category_id = product['category']
-                category = Category.objects.get(categoryId=category_id)
+                try:
+                    category = Category.objects.get(categoryId=category_id)
+                except Category.DoesNotExist:
+                    # Create a new category with default values
+                    category = Category.objects.create(
+                        categoryId=category_id,
+                        name=f'{category_id}-NAME',
+                        description=f'{category_id}-DESCRIPTION',
+                        icon='IconMoodLookUp'
+                    )
                 
                 product_defaults = {
                     'name': product['name'],
@@ -196,6 +205,7 @@ class ExportImportProductView(APIView):
             return Response({
                 'message': f'An error occurred: {str(e)}'
             }, status=400)
+
 
 class createCategory(APIView):
     permission_classes = [IsAuthenticated]
