93b623cc0fab74b698542d6dc59170e1772007e0
kojicc <syjeikoo@gmail.com>
2024-10-26 15:57:10 +0800

    frontend config updates

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,5 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
+import axios from './utils/axiosInstance';
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
@@ -10,3 +11,22 @@ export default withBundleAnalyzer({
     ignoreDuringBuilds: true,
   },
 });
+
+export async function headers() {
+  return [
+    {
+      // matching all API routes
+      source: axios,
+      headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value:
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        },
+      ],
+    },
+  ];
+}
