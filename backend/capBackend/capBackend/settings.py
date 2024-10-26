a8c3b0badcfb6a82ad3f8257f5745e6eb3d79795
kojicc <syjeikoo@gmail.com>
2024-10-26 15:39:34 +0800

    settings update

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -209,7 +209,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
     'https://capstoners.vercel.app',  # Replace with your frontend domain
     'http://localhost:3000',
-    'https://capstoners-665f8af6ebcf.herokuapp.com/'  # Add this for local development
+    'https://capstoners-665f8af6ebcf.herokuapp.com'  # Add this for local development
 ]
 # para maallow frontend maaccess cookies kase naglologin thru cookies
 CORS_ALLOW_CREDENTIALS = True
