90ccc8d70816753a41f88dec1b647177855a2f9b
kojicc <syjeikoo@gmail.com>
2024-10-27 00:42:37 +0800

    zxcawsd

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -105,17 +105,20 @@ WSGI_APPLICATION = 'capBackend.wsgi.application'
 
 #somehow nagana kahit di nagamit neto
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL) if DATABASE_URL else {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cthmDB',
-        'USER': 'root',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+    'default': dj_database_url.config(default=DATABASE_URL)
+ }
 
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'cthmDB',
+#         'USER': 'root',  #  MySQL username to
+#         'PASSWORD': '12345',  #  MySQL password to
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+    
+# }
 
 # DATABASES = {
 #     'default': {
