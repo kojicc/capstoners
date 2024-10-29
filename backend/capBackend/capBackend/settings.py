8b9655b187c4fc53cebf0a5c1fd79dafe97841a8
kojicc <syjeikoo@gmail.com>
2024-10-29 16:16:41 +0800

    Update settings.py

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -108,17 +108,6 @@ WSGI_APPLICATION = 'capBackend.wsgi.application'
 #     'default': dj_database_url.config(default=DATABASE_URL)
 #  }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cthmDB',
-#         'USER': 'root',  #  MySQL username to
-#         'PASSWORD': '12345',  #  MySQL password to
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-    
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
