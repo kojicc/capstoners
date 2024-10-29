e12603537b1a763e92783daa2922a1d4d4f94e34
kojicc <syjeikoo@gmail.com>
2024-10-30 00:31:52 +0800

    qwasdwasd

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -33,6 +33,7 @@ SECRET_KEY = 'django-insecure-)91p!o&_y1&fx1a@uc^vdk7$f$g#12kpngbf(q5-3v!b95ga%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['https://capstoners.vercel.app','http://localhost:3000','capstoners-665f8af6ebcf.herokuapp.com','127.0.0.1','web-production-c92c.up.railway.app']
 
 
 # Application definition
