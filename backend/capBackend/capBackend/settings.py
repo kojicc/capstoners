d1a9eece19274372d85f62033b8e3d4080405b74
kojicc <syjeikoo@gmail.com>
2024-11-06 04:10:25 +0800

    adaas

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-)91p!o&_y1&fx1a@uc^vdk7$f$g#12kpngbf(q5-3v!b95ga%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://capstoners.vercel.app','http://localhost:3000','capstoners-665f8af6ebcf.herokuapp.com','127.0.0.1','https://capstoners-backend.vercel.app']
+ALLOWED_HOSTS = ['https://capstoners.vercel.app','http://localhost:3000','capstoners-665f8af6ebcf.herokuapp.com','127.0.0.1','.vercel.app']
 
 
 # Application definition
@@ -185,6 +185,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "https://capstoners.vercel.app",
+    "https://capstoners-backend.vercel.app"
 ]
 
 # Gmail settings
