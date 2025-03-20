77b143ebee800b4b09d2d23af0a96005dcc20b94
kojicc <syjeikoo@gmail.com>
2025-03-20 19:23:18 +0800

    settings change

--- backend/capBackend/capBackend/settings.py
+++ backend/capBackend/capBackend/settings.py
@@ -116,8 +116,7 @@ DATABASES = {
         'NAME': 'cthmDB_AWS',
         'USER': 'ben10',
         'PASSWORD': 'cthmDB2024!',
-        'HOST': 'cthmdb.cvy2ccc2o9x0.ap-southeast-1.rds.amazonaws.com',
-        # 'HOST': 'cthm-db.cr04mi4yu3a2.ap-southeast-1.rds.amazonaws.com',
+        'HOST': 'cthmdb.cti06g6uca7v.ap-southeast-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
