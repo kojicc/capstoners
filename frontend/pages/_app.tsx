9b2a4ed6f440601836c921be3b620dc7e49a9dfe
kojicc <syjeikoo@gmail.com>
2025-05-20 07:33:42 +0800

    yehey

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -38,7 +38,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 name="viewport"
                 content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
               />
-              <link rel="shortcut icon" href="/lake.png" />
+              <link rel="shortcut icon" href="/cthm_icon.png" />
             </Head>
             <Component {...pageProps} />
           </ModalsProvider>
